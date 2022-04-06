a76eff5ca8cbdda175e06aa6bdbd84a40a45a327
Justin Park <79668170+rpcagithub@users.noreply.github.com>
2022-04-06 13:29:57 -0400

    unimport firebase

--- ReactNativeTS_Learning/App.tsx
+++ ReactNativeTS_Learning/App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native';
-import { app, db } from './src/constants/firebase';
-import { collection, doc, getDoc, getDocs } from "firebase/firestore"; 
+// import { app, db } from './src/constants/firebase';
+// import { collection, doc, getDoc, getDocs } from "firebase/firestore"; 
 
 export default function App() {
   function shake (): void {
     console.log("shaken");
+  }
 
-    readData();
+    // readData();
 
     // const docRef = doc(db, "output", "help");
     // const docSnap = getDoc(docRef);
@@ -16,14 +17,13 @@ export default function App() {
     // const querySnapshot = getDocs(collection(db, "Output"));
     // console.log(querySnapshot);
 
-  }
+  // }
 
-  const readData = async () => {
-    let docRef = collection(db, 'Output');
-    console.log(docRef);
-    const docSnap = await doc(docRef, 'Hoj4h4WH7TX10ywP5WBF');
-    console.log(docSnap);
-  }
+  // const readData = async () => {
+  //   let docRef = collection(db, 'Output');
+  //   console.log(docRef);
+  //   const docSnap = await doc(docRef, 'Hoj4h4WH7TX10ywP5WBF');
+  //   console.log(docSnap);
 
   return (
     <View style={styles.container}>
