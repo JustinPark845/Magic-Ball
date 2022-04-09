276d16dcf28dfd7ac9b591f9ee2038b71dbd102c
Justin Park <79668170+rpcagithub@users.noreply.github.com>
2022-04-09 14:07:36 -0400

    Update

--- ReactNativeTS_Learning/App.tsx
+++ ReactNativeTS_Learning/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, Animated} from 'react-native';
 import React, { useState } from 'react';
+import { useTransition } from 'react-native-redash';
 // import { app, db } from './src/constants/firebase';
 // import { collection, doc, getDoc, getDocs } from "firebase/firestore"; 
 
@@ -26,16 +27,7 @@ export default function App() {
   }
 
   function shakeAnimation (isActive: boolean): void {
-    if (isActive) {
-      Animated.loop(
-        Animated.timing(shakeAnimationValue, { toValue: 10, duration: 100, useNativeDriver: true}),
-        // Animated.timing(shakeAnimationValue, { toValue: -10, duration: 100, useNativeDriver: true }),
-        // Animated.timing(shakeAnimationValue, { toValue: 10, duration: 100, useNativeDriver: true }),
-        // Animated.timing(shakeAnimationValue, { toValue: 0, duration: 100, useNativeDriver: true })
-      )
-    } else {
-// turn off
-    }
+
   }
 
   function revealLoading (): void {
@@ -51,7 +43,6 @@ export default function App() {
     setMagic8Response(magic8BallResponses[Math.floor(Math.random()*magic8BallResponses.length)]);
   }
 
-  const shakeAnimationValue = new Animated.Value(0);
   const magic8BallResponses: string[] = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
   const inputDataDelay: Parameter = {
     data: {
